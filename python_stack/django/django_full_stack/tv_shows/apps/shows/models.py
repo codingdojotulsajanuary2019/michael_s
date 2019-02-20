f2f0a390286340e022c1d473c4bf993942cdd43c
Michael Shea <MichaelShea1814@gmail.com>
2019-02-20 07:53:35 -0600

    adding django tv shows project with vailidations

--- python_stack/django/django_full_stack/tv_shows/apps/shows/models.py
+++ python_stack/django/django_full_stack/tv_shows/apps/shows/models.py
@@ -1,4 +1,19 @@
 from django.db import models
+import datetime
+
+
+class ShowManager(models.Manager):
+    def show_vaildator(self, postData):
+        errors = {}
+
+        if len(postData['showtitle']) < 2:
+            errors['showtitle'] = "Title should be at least 2 Characters Long"
+        if len(postData['shownetwork']) < 1:
+            errors['shownetwork'] = "Field cannot be empty"
+        if len(postData['showdescription']) < 10:
+            errors['showdescription'] = "Show Description must be longer than 10 characters"
+
+        return errors
 
 class Shows(models.Model):
     title = models.CharField(max_length = 255)
@@ -8,5 +23,10 @@ class Shows(models.Model):
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
+    objects = ShowManager()
+
     def __repr__(self):
         return f"id: {self.id}, Title: {self.title}, Network:  {self.network}, Release Date: {self.release_date}, Description: {self.description}, Created At: {self.created_at}, Updated At: {self.updated_at} "
+
+
+
--- python_stack/django/django_full_stack/tv_shows/apps/shows/views.py
+++ python_stack/django/django_full_stack/tv_shows/apps/shows/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Shows
+from .models import *
+from django.contrib import messages
 
 # SHOW TABLE OF ALL SHOWS
 def allshows(request):
@@ -18,9 +19,17 @@ def show_add_show(request):
 
 # ADD A NEW SHOW
 def add_new_show(request):
-    Shows.objects.create(title = request.POST['showtitle'], network = request.POST['shownetwork'], release_date = request.POST['releasedate'], description = request.POST['showdescription'])
+    errors = Shows.objects.show_vaildator(request.POST)
+    print(errors)
+    if len(errors) > 0:
+        for tag, message in errors.items():
+            messages.error(request, message, extra_tags = tag)
+        return redirect('/shows/new')
+    else:
 
-    return redirect('/')
+        Shows.objects.create(title = request.POST['showtitle'], network = request.POST['shownetwork'], release_date = request.POST['releasedate'], description = request.POST['showdescription'])
+
+        return redirect('/')
 
 #SHOW TO SPECIFIC SHOW BY ID
 def show_show(request, id):
@@ -42,12 +51,20 @@ def show_edit_show(request, id):
 #ROUTE TO UPDATE SHOW INFO
 def update_show(request, id):
     show_id = int(id)
-    update_show = Shows.objects.get(id=show_id)
-    update_show.title = request.POST['showtitle']
-    update_show.network = request.POST['shownetwork']
-    update_show.release_date = request.POST['releasedate']
-    update_show.description = request.POST['showdescription']
-    update_show.save()
+    errors = Shows.objects.show_vaildator(request.POST)
+    print(errors)
+    if len(errors) > 0:
+        for tag, message in errors.items():
+            messages.error(request, message, extra_tags = tag)
+        return redirect(f'/shows/{show_id}/edit')
+    else:
+        show_id = int(id)
+        update_show = Shows.objects.get(id=show_id)
+        update_show.title = request.POST['showtitle']
+        update_show.network = request.POST['shownetwork']
+        update_show.release_date = request.POST['releasedate']
+        update_show.description = request.POST['showdescription']
+        update_show.save()
 
     return redirect ('/')
 
