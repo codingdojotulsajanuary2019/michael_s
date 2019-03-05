29058822f64c5bf54ccce4c563fabbdfe8af2ed5
Michael Shea <MichaelShea1814@gmail.com>
2019-03-04 21:58:19 -0600

    adding boxing/unboxing

---
+++ c#/Fundamentals/Multiplication_Table/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] MultTable = new int[10];
+            for(int i = 1; i < MultTable.Length; i++)
+            {
+                MultTable[i-1] = i;
+                Console.Write(MultTable[i-1] + " , ");
+            }
+
+        }
+    }
+}
---
+++ c#/Fundamentals/unboxing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> MyList = new List<object>();
+            MyList.Add(7);
+            MyList.Add(28);
+            MyList.Add(-1);
+            MyList.Add(true);
+            MyList.Add("Chair");
+
+            int sum = 0;
+            foreach(var item in MyList)
+            {
+                Console.WriteLine(item);
+                if(item is int)
+                {
+                    sum = sum + (int)item;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
