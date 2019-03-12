696806c08d035a44b569233d38e5c673c89f30d0
Michael Shea <MichaelShea1814@gmail.com>
2019-03-12 13:00:53 -0500

    added Random Passcode generator with session count

---
+++ c#/ASPMVC2/RandomPasscode/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RandomPasscode.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace RandomPasscode.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            if(HttpContext.Session.GetInt32("count")== null)
+            {
+                HttpContext.Session.SetInt32("count", 0);
+            }
+            else
+            {
+                int? CodeCount = HttpContext.Session.GetInt32("count");
+                HttpContext.Session.SetInt32("count", (int)++CodeCount);
+            }
+            Code MyCode = new Code();
+            string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            Random rand = new Random();
+            for(int i = 0; i < 14 ; i++)
+            {
+                var AlphaNum = Chars[rand.Next(Chars.Length)];
+                MyCode.RandomCode[i] = AlphaNum.ToString();
+            }
+            ViewBag.count = HttpContext.Session.GetInt32("count");
+            
+            return View("Index", MyCode);
+        }
+
+    }
+}
---
+++ c#/ASPMVC2/RandomPasscode/Models/Code.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace RandomPasscode.Models
+{
+    public class Code
+    {
+        public string[] RandomCode = new string[14];
+
+
+    }
+}
